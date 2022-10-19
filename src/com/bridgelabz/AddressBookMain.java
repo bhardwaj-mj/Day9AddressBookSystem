36bddd8bef048de08c393c9ab6188a748b885616
bhardwaj-mj <bhardwajmanoj555@gmail.com>
2022-10-19 20:40:55 +0530

    Added:Multiple address book to the system

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -6,13 +6,28 @@ public class AddressBookMain {
     void openContact() {
         int choice;
         AddressBook addressBook=new AddressBook();
+        AddressBook book1 = new AddressBook();
+        AddressBook book2 = new AddressBook();
+        AddressBook book3 = new AddressBook();
+        addressBook.addressBooks.put("AddressBook",addressBook);
+        addressBook.addressBooks.put("AddressBook1", book1);
+        addressBook.addressBooks.put("AddressBook2", book2);
+        addressBook.addressBooks.put("AddressBook3", book3);
         do{
             Scanner inputChoice = new Scanner(System.in);
             System.out.println("Enter your choice\n1: Add Contact\t2: Edit Contact\n3: Display Contact\t4: Delete Contact");
             choice = inputChoice.nextInt();
             switch (choice) {
                 case 1:
-                    addressBook.getContact();
+                    System.out.println("Enter:\n1:Address Book\t2:Address Book1\n3:Address Book2\t4:Address Book3");
+                    int chooseAddressBook=inputChoice.nextInt();
+                    if(chooseAddressBook==1){addressBook.getContact();}
+                    else if(chooseAddressBook==2){book1.getContact();}
+                    else if(chooseAddressBook==3){book2.getContact();}
+                    else if(chooseAddressBook==4){book3.getContact();}
+                    else {
+                        System.out.println("Option not found");
+                    }
                     break;
                 case 2:
                     addressBook.editContact();
